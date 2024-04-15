e185ad2445a867e052306ff1b12f906f1c5bea6c
Weriqui <114047309+Weriqui@users.noreply.github.com>
2024-04-15 11:28:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def pesquisar():
         return jsonify({'error': 'Erro ao obter o resultado innerText'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
